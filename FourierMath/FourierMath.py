4b9cceb8997b8f526624e1a547e7d9259b70005d
ruanluyu <rly_pxx_123@sina.com>
2018-08-08 14:44:16 +0900

    20180808

--- FourierMath/FourierMath.py
+++ FourierMath/FourierMath.py
@@ -38,85 +38,15 @@ def numSolve(m,cs,ce,pts):#主要计算函数2-贝塞尔曲线方程代入
     return (-pts[0]+3*pts[1]-3*pts[2]+pts[3])*prSolve(m,cs,ce,3)+3*(pts[0]-2*pts[1]+pts[2])*prSolve(m,cs,ce,2)+3*(-pts[0]+pts[1])*prSolve(m,cs,ce,1)+pts[0]*prSolve(m,cs,ce,0)
 
 
+def cpToList(cp):
+    return [cp.real,cp.imag]
 start = 0
 end = 1000#圆圈数量
 points = []#贝塞尔采集点
 out = []#输出用坐标容器
 center = [500,500]#中心点位置
 curWeight = []#各段曲线的时间权重容器
-
-def cpToList(cp):
-    return [cp.real,cp.imag]
-
 trDatas = []#临时容器
-with open('rawvertexes.txt', 'r') as f:#读取并解析SVG信息
-    rawdata = f.readlines()
-    curve = re.sub(r'\s','',"".join(rawdata))
-    cells = re.findall(r'\w[\d\,\-\.]+',curve)
-    for cell in cells:
-        trcdata = []
-        formatString = re.sub(r'-',',-',cell)
-        trcdata.append(re.match(r'[A-Za-z]',formatString).group(0))
-        rawvers = re.sub(r'[A-Za-z]\,?','',formatString).split(',')
-        for st in range(len(rawvers)):
-            rawvers[st] = float(rawvers[st])
-        vergroup = []
-        vercurgrp = []
-        for st in range(len(rawvers)):
-            vercurgrp.append(rawvers[st])
-            if len(vercurgrp)>=2:
-                vergroup.append(vercurgrp[0]+vercurgrp[1]*1j)
-                vercurgrp.clear()
-        if len(vercurgrp)>0:
-            if re.match(r'v',trcdata[0],re.I):
-                vergroup.append(0+vercurgrp[0]*1j)
-            elif re.match(r'h',trcdata[0],re.I):
-                vergroup.append(vercurgrp[0]+0j)
-        trcdata.append(vergroup)
-        trDatas.append(trcdata)
-    print('Vertexes data have been read.')
-
-for i in range(1,len(trDatas)):#解析SVG信息
-    if re.match(r'[a-z]',trDatas[i][0]):
-        for j in range(len(trDatas[i][1])):
-            trDatas[i][1][j]+=trDatas[i-1][1][-1]
-for i in range(len(trDatas)):#解析SVG信息
-    flag = trDatas[i][0]
-    if re.match(r'm',flag,re.I):continue
-    trDatas[i][1].insert(0,trDatas[i-1][1][-1])
-    if re.match(r's',flag,re.I):
-        trDatas[i][1].insert(1,2*trDatas[i-1][1][-1]-trDatas[i-1][1][-2])
-    if re.match(r'[lvh]',flag,re.I):
-        trDatas[i][1].insert(1,trDatas[i][1][0]/3+trDatas[i][1][-1]*2/3)
-        trDatas[i][1].insert(1,trDatas[i][1][0]*2/3+trDatas[i][1][-1]/3)
-
-for i in range(len(trDatas)):#解析SVG信息
-    flag = trDatas[i][0]
-    if re.match(r'm',flag,re.I):continue
-    points.append(trDatas[i][1])
-for i in range(len(points)):#将中心点归零
-    for j in range(len(points[i])):
-        points[i][j] -= center[0]+1j*center[1]
-
-print("Weight process start.")#计算时间权重
-wsum = 0
-for curve in points:#Calculate weight
-    wst = 10 #steps
-    sum = 0
-    for i in range(1,wst):
-        sum+=abs(bezier(linear(i,0,wst,0,1),curve[0],curve[1],curve[2],curve[3])-
-        bezier(linear(i-1,0,wst,0,1),curve[0],curve[1],curve[2],curve[3]))
-    curWeight.append(sum)
-    wsum+=sum
-for i in range(len(curWeight)):
-    curWeight[i] /= wsum
-for i in range(1,len(curWeight)):
-    curWeight[i] += curWeight[i-1]
-curWeight.insert(0,0)
-curWeight[-1] = 1
-print("Weight process finished.")
-#print(curWeight)
-
 def mainCalculation(s):
     m = 0
     if s>0: 
@@ -129,18 +59,87 @@ def mainCalculation(s):
         sum += numSolve(m,cs,ce,points[i])
     return cpToList(sum)
 
-print("Main calculation start.")
-out = []
-if __name__ == '__main__':#并行计算开始
-    multiprocessing.freeze_support()
+
+if __name__ == '__main__':
+    with open('rawvertexes.txt', 'r') as f:#读取并解析SVG信息
+        rawdata = f.readlines()
+        curve = re.sub(r'\s','',"".join(rawdata))
+        cells = re.findall(r'\w[\d\,\-\.]+',curve)
+        for cell in cells:
+            trcdata = []
+            formatString = re.sub(r'-',',-',cell)
+            trcdata.append(re.match(r'[A-Za-z]',formatString).group(0))
+            rawvers = re.sub(r'[A-Za-z]\,?','',formatString).split(',')
+            for st in range(len(rawvers)):
+                rawvers[st] = float(rawvers[st])
+            vergroup = []
+            vercurgrp = []
+            for st in range(len(rawvers)):
+                vercurgrp.append(rawvers[st])
+                if len(vercurgrp)>=2:
+                    vergroup.append(vercurgrp[0]+vercurgrp[1]*1j)
+                    vercurgrp.clear()
+            if len(vercurgrp)>0:
+                if re.match(r'v',trcdata[0],re.I):
+                    vergroup.append(0+vercurgrp[0]*1j)
+                elif re.match(r'h',trcdata[0],re.I):
+                    vergroup.append(vercurgrp[0]+0j)
+            trcdata.append(vergroup)
+            trDatas.append(trcdata)
+        print('Vertexes data have been read.')
+
+    for i in range(1,len(trDatas)):#解析SVG信息
+        if re.match(r'[a-z]',trDatas[i][0]):
+            for j in range(len(trDatas[i][1])):
+                trDatas[i][1][j]+=trDatas[i-1][1][-1]
+    for i in range(len(trDatas)):#解析SVG信息
+        flag = trDatas[i][0]
+        if re.match(r'm',flag,re.I):continue
+        trDatas[i][1].insert(0,trDatas[i-1][1][-1])
+        if re.match(r's',flag,re.I):
+            trDatas[i][1].insert(1,2*trDatas[i-1][1][-1]-trDatas[i-1][1][-2])
+        if re.match(r'[lvh]',flag,re.I):
+            trDatas[i][1].insert(1,trDatas[i][1][0]/3+trDatas[i][1][-1]*2/3)
+            trDatas[i][1].insert(1,trDatas[i][1][0]*2/3+trDatas[i][1][-1]/3)
+
+    for i in range(len(trDatas)):#解析SVG信息
+        flag = trDatas[i][0]
+        if re.match(r'm',flag,re.I):continue
+        points.append(trDatas[i][1])
+    for i in range(len(points)):#将中心点归零
+        for j in range(len(points[i])):
+            points[i][j] -= center[0]+1j*center[1]
+
+    print("Weight process start.")#计算时间权重
+    wsum = 0
+    for curve in points:#Calculate weight
+        wst = 10 #steps
+        sum = 0
+        for i in range(1,wst):
+            sum+=abs(bezier(linear(i,0,wst,0,1),curve[0],curve[1],curve[2],curve[3])-
+            bezier(linear(i-1,0,wst,0,1),curve[0],curve[1],curve[2],curve[3]))
+        curWeight.append(sum)
+        wsum+=sum
+    for i in range(len(curWeight)):
+        curWeight[i] /= wsum
+    for i in range(1,len(curWeight)):
+        curWeight[i] += curWeight[i-1]
+    curWeight.insert(0,0)
+    curWeight[-1] = 1
+    print("Weight process finished.")
+    #print(curWeight)
+    print("Main calculation start.")
+    out = []
+    multiprocessing.freeze_support()#并行计算开始
     pool = multiprocessing.Pool()
     out = list(pool.map(mainCalculation, range(start,end+1)))
     pool.close()
     pool.join()
-print("Main calculation finished.")
-File = open("datas.txt", "w")#把结果写入硬盘
-for dc in out:
-    File.write("{0}".format(dc))
-    File.write("\n")
-File.close()
-print("Data saved.\n\nWork finished.\n")#完成提示
+    print("Main calculation finished.")
+    File = open("datas.txt", "w")#把结果写入硬盘
+    for dc in out:
+        File.write("{0}".format(dc))
+        File.write("\n")
+    File.close()
+    print("Data saved.\n\nWork finished.\n")#完成提示
+

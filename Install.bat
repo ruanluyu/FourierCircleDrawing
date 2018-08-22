a75997933a89f0eab150f6687eac0c9f870bd9f3
ruanluyu <rly_pxx_123@sina.com>
2018-08-23 02:14:07 +0900

    Auto update

---
+++ Install.bat
@@ -0,0 +1,6 @@
+mkdir FourierCircleDrawing
+
+cd FourierCircleDrawing  
+git init
+git remote add origin https://github.com/ruanluyu/FourierCircleDrawing.git
+git pull origin master
---
+++ UpdateProject.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "Auto update"
+git push origin master

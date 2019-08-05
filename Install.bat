1cece8eeb1a29d859c9ada82a520f0dc040f0aac
ruanluyu <rly_pxx_123@sina.com>
2019-08-06 00:39:52 +0900

    clear doc branch

--- Install.bat
+++
@@ -1,4 +0,0 @@
-mkdir FourierCircleDrawing
-
-cd FourierCircleDrawing  
-git clone https://github.com/ruanluyu/FourierCircleDrawing.git
--- UpdateProject.bat
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -m "Auto update"
-git push origin master
